248016811393473f44c4cfdaae8e56914124a02e
unknown <soeun8636@naver.com>
2023-07-15 22:13:42 +0900

    url 수정

--- src/main/java/Alumni/backend/module/controller/PostController.java
+++ src/main/java/Alumni/backend/module/controller/PostController.java
@@ -25,14 +25,14 @@ public class PostController {
 
     private final PostService postService;
 
-    @PostMapping("/post/create")
+    @PostMapping("/create")
     public ResponseEntity<? extends BasicResponse> postCreate(@CurrentUser Member member, @RequestBody @Valid PostCreateRequestDto postCreateRequestDto){
         postService.postCreate(member, postCreateRequestDto);
 
         return ResponseEntity.ok().body(new SingleResponse("게시글 등록 완료"));
     }
 
-    @PutMapping("/post/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<? extends BasicResponse> postModify(@CurrentUser Member member, @PathVariable("id") Long postId,
                                                               @RequestBody @Valid PostModifyRequestDto postModifyRequestDto){
         postService.postModify(member, postId, postModifyRequestDto);
@@ -40,7 +40,7 @@ public class PostController {
         return ResponseEntity.ok().body(new SingleResponse("게시글 수정 완료"));
     }
 
-    @DeleteMapping("/post/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<? extends  BasicResponse> postDelete(@CurrentUser Member member, @PathVariable("id") Long postId){
         postService.postDelete(member, postId);
 
